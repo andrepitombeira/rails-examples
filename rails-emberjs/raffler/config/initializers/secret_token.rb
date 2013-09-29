662f69cc064b3c7a7b88011d61ea14ce93f4aa45
andrepitombeira <andrepitombeira@gmail.com>
2013-09-28 23:05:02 -0300

    create a project in rails with emberjs

---
+++ rails-emberjs/raffler/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = '8af3e9b3565600776b2f00f6de31c27e5e9807b6f958977923b8f8c47596f32d9139baaf41a09d91a7ba15db4c65c66417f2d6ba9bdcd8250b0bf57d39de44f1'
